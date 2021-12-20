0dc8d52d91ccca31f659874363004057080be5df
GrGrimales <grojasgrimales@gmail.com>
2021-12-20 12:08:01 -0500

    Se elimino comentarios de AppRouter

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,23 +1,3 @@
-// // import React from "react";
-// // import { Routes, Route, Link } from "react-router-dom";
-// // import { Portfolio } from "../components/pages/Portfolio";
-// // import { Herramientas } from "../components/pages/Herramientas";
-// // import { Contacto } from "../components/pages/Contacto";
-// // import Home from "../Home";
-
-// // export const AppRouter = () => {
-// //   return (
-
-// //       <Routes>
-// //         <Route  path="/" element={<Home/>} />
-// //         <Route  path="/portfolio" element={<Portfolio/>} />
-// //         <Route  path="/herramientas" element={<Herramientas/>} />
-// //         <Route  path="/contacto" element={<Contacto/>} />
-
-// //       </Routes>
- 
-      
-// //   );
 
 import {
   Routes,
